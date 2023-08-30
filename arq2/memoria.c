b8b33527a3d61ea940a307e8f045ff7d21a2d1a1
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-08-30 17:32:12 -0300

    Update memoria.c

--- arq2/memoria.c
+++ arq2/memoria.c
@@ -7,6 +7,30 @@ unsigned int *memoria;
 
 void inicializaMemoria(int m){
     memoria = (unsigned int*)malloc(sizeof(unsigned int) * m);
+}#include <stdio.h>
+#include <stdlib.h>
+#include "memoria.h"
+#include "tradutor.h"
+#include "common.h"
+unsigned int *memoria;
+
+void inicializaMemoria(int m){
+    memoria = (unsigned int*)malloc(sizeof(unsigned int) * m);
+}
+
+void insereMemoria(unsigned int instrucao){
+    if(memoria==NULL){
+        printf("Erro.");
+    }
+    else{
+        memoria[pc++]=instrucao;
+    }
+}
+
+void printMemoria(int memsize){
+    for(int i=0; i<memsize; i++){
+        printf("\n%u", memoria[i]);
+    }
 }
 
 void insereMemoria(unsigned int instrucao){
