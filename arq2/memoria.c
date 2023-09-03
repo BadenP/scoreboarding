ce5295fc380845b8c77318950158054ca62bf280
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:12:16 -0300

    Update memoria.c

--- arq2/memoria.c
+++ arq2/memoria.c
@@ -4,27 +4,28 @@
 #include "tradutor.h"
 #include "registradores.h"
 
-unsigned int *memoria;
+long int *memoria;
 int tam;
+int qtdeInsts;
 
 void inicializaMemoria(int m){
     if(m<=100){
         printf("Tamanho de memória insuficiente. O tamanho da memória deve ser mair do que 100. \n");
     }
     else{
-        memoria = (unsigned int*)malloc(sizeof(unsigned int) * m);
+        memoria = (long int*)malloc(sizeof(long int) * m);
         tam=m;
     }
 }
 
 void printMemoria(){
     printf("Conteúdo da memória:\n");
-    for (int i = 0; i < tam; i++) {
-        printf("Endereço %d: %u\n", i, memoria[i]);
+    for (int i = 0; i < tam; i = i+4) {
+        printf("Endereço %d: %ld\n", i, memoria[i]);
     }
 }
 
-void insereMemoria(unsigned int instrucao){
+void insereMemoria(long int instrucao){
     if(memoria==NULL){
         printf("Erro.");
     }
