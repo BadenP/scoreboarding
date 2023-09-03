56918adac8b0bf089fadba54789538e1ff4d9529
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:13:50 -0300

    Update processor.h

--- arq2/processor.h
+++ arq2/processor.h
@@ -7,24 +7,15 @@
 #include "registradores.h"
 #include "memoria.h"
 
-typedef struct {
-    int busca;
-    int emissao;
-    int leitura_op;
-    int * execucao[3];
-    int escrita;
-}pipeline;
-
-extern pipeline pipe;
-
 void inicializaPipeline(int add, int mul, int inteiro);
-int buscaInstrucao(pipeline pipe, int pc, int barramento);
 unsigned int getOpcode(unsigned int instrucao);
 unsigned int getRegistradorDestino(unsigned int instrucao);
 unsigned int getRegistradorFonte1(unsigned int instrucao);
 unsigned int getRegistradorFonte2(unsigned int instrucao);
-void emiteInstrucao(pipeline *pipe, conjuntoUFS *UFS);
-void leituraOperandos(pipeline *pipe);
-void executaInstrucao(pipeline *pipe);
+void emiteInstrucao();
+void buscaInstrucao();
+void leituraDeOperandos();
+void execucao();
+void executaInstrucao(int destino, int fonte1, int fonte2, int opcode);
 
 #endif 
