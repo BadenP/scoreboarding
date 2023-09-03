737d11a45ca2c67c4d5e50d593ea43261ed072e3
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 14:57:24 -0300

    Add files via upload

---
+++ scoreboardingSEP-2/arq2/scoreboarding.h
@@ -0,0 +1,20 @@
+#ifndef SCOREBOARDING_H
+#define SCOREBOARDING_H
+
+void statusRegistradores();
+void statusUFs();
+
+typedef struct{
+    int instrucao;
+    int busca;
+    int emissao;
+    int leitura_op;
+    int execucao;
+    int escrita;
+}statusInstrucoes;
+
+extern statusInstrucoes *statusI;
+
+void inicializaStatusInstrucoes();
+
+#endif
