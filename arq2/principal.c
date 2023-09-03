07aceb21446f08d4f86b9a64a45267ae539711af
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:13:08 -0300

    Update principal.c

--- arq2/principal.c
+++ arq2/principal.c
@@ -1,19 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+#include "processor.h"
 #include <string.h>
 
 
 int main(int argc, char *argv[]){
 
 	if((strcmp(argv[1], "-p") == 0) && (strcmp(argv[3], "-m") == 0)){
-
+        if(atoi(argv[4])<101){
+          printf("\nErro ao carregar memória: deve ser maior que 100\n");
+          return 0;
+        }
         //Esta função lê o programa e o carrega para a memória
         //Memória é inicializada 
         //Banco de registradores é inicializado
         //UFS são inicializadas
+        //Status das instruções é inicializado
         //Qtde de ciclos para executar de cada instrução são salvas em um vetor
-        leituraArquivo(argv[2],atoi(argv[4]),argv[6]);
+        if(!leituraArquivo(argv[2],atoi(argv[4]),argv[6])){
+          return 0;
+        }
+        pc = 0;
+        clock = 1;
+        buscaInstrucao();
+        clock++;
+        emiteInstrucao();
     }
     else{
         printf("Erro ao executar o programa.\n");
