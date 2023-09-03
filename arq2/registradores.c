1d0f85eb5af688452994baefbc67b38b414d0fd6
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:14:37 -0300

    Update registradores.c

--- arq2/registradores.c
+++ arq2/registradores.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 unsigned int bancoRegs[32];
 int pc=0;
+int ir;
 
-void escreverRegistrador();
+void escreverRegistrador(int indice, int valor){
+    bancoRegs[indice] = valor;
+};
 
-void lerRegistrador();
+int lerRegistrador(int indice){
+    return bancoRegs[indice];
+}
+
+void armazenarValorRegistrador(const char *linha, int tamanho) {
+    char *separador = strstr(linha, "=");
+    if (separador != NULL) {
+        // Extrai o número do registrador (X) da parte esquerda da linha.
+        int registrador_numero;
+        sscanf(linha, "r%d =", &registrador_numero);
+
+        // Verifica se o número do registrador é válido (1 a tamanho).
+        if (registrador_numero >= 1 && registrador_numero < tamanho) {
+            // Extrai o valor (Y) da parte direita da linha.
+            int valor;
+            sscanf(separador + 1, "%d", &valor);
+
+            // Armazena o valor no registrador correspondente.
+            bancoRegs[registrador_numero] = valor;
+        }
+    }
+}
