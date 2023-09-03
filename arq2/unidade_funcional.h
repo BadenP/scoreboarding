8b64fd4887a0fcda0d2072e8aeac98aaa90e113c
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:17:55 -0300

    Update unidade_funcional.h

--- arq2/unidade_funcional.h
+++ arq2/unidade_funcional.h
@@ -2,6 +2,7 @@
 #define UNIDADE_FUNCIONAL_H
 
 #include <stdbool.h>
+#include "processor.h"
 
 typedef enum { ADD, MUL, INT } tipoUF;
 
@@ -14,8 +15,8 @@ typedef struct UF {
     unsigned int fk;
     struct UF* qj;
     struct UF* qk;
-    bool rj;
-    bool rk;
+    int rj;
+    int rk;
     int qtde_ciclos;
 } UF;
 
@@ -26,15 +27,15 @@ typedef struct {
     int qtdeADD, qtdeMUL, qtdeINT;
 } conjuntoUFS;
 
-extern UF * vetorResultados; //Esse vetor serve para mostrar quais registradores de destino vão ser escritos ao final da execução 
+extern UF* vetorResultados[32]; //Esse vetor serve para mostrar quais registradores de destino vão ser escritos ao final da execução 
 extern conjuntoUFS unidadesFuncionais;
 
 void printConjuntoUFS(conjuntoUFS* conjunto);
 void printUF(UF* uf);
 void inicializaConjuntoUFs(conjuntoUFS* UFS, int add, int mul, int inteiro);
-int getUFdisponivel(UF* uf);
+int getUFdisponivel(int tipo);
 int ufDisponivel(tipoUF tipo, conjuntoUFS *UFS);
-tipoUF getUF(unsigned int instrucao);
+int getTipoUF(unsigned int instrucao);
 
 #endif // UNIDADEFUNCIONAL_H
 
