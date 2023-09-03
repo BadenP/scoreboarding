0631295364a20a804644e98592b3c319c4441e90
lucamattosinho <123836135+lucamattosinho@users.noreply.github.com>
2023-09-03 12:11:47 -0300

    Update main.h

--- arq2/main.h
+++ arq2/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 extern int ciclosParaExecutar[16];
+extern int clock;
+extern int qtdeAdd, qtdeInt, qtdeMul;
+extern int addCiclos, mulCiclos, lwCiclos, subCiclos, divCiclos, swCiclos, bgtCiclos, jCiclos;
+extern int addiCiclos, subiCiclos, andCiclos, orCiclos, notCiclos, bltCiclos, beqCiclos, bneCiclos;
 int getValor(const char *linha);
-void leituraArquivo(char * file, int memsize, char * output);
+int leituraArquivo(char * file, int memsize, char * output);
 #endif
